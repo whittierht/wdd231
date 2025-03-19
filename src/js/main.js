e42db9dd562cf4a1628e0d86aa4decd3207e0363
Hyrum Whittier <whittierht@gmail.com>
2025-03-19 11:45:11 -0600

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -19,11 +19,38 @@ function setParkInfoLinks(data) {
 
 async function init() {
   const parkData = await getParkData();
-
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(parkInfoLinks);
 }
+
+function enableNavigation() {
+  const menuButton = document.querySelector("#global-nav-toggle");
+  
+
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target;
+
+  document.querySelector(".global-nav").classList.toggle("show");
+
+  if (target.tagName != "BUTTON") {
+    target = target.closest("button");
+  }
+
+  if (document.querySelector(".global-nav").classList.contains("show")) {
+
+    target.setAttribute("aria-expanded", true);
+  } else {
+
+    target.setAttribute("aria-expanded", false);
+  }
+
+  console.log("toggle");
+  });
+}
+enableNavigation();
+
+
 
 init();
 
@@ -31,8 +58,6 @@ init();
 setIntro(parkData);
 setParkInfoLinks(parkInfoLinks);
 setHeaderFooter(parkData);
-
-
 
 
 
