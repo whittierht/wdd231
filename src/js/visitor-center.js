1ed3254e9bec6c06449f03a96fc226205a1b397b
Hyrum Whittier <whittierht@gmail.com>
2025-03-29 12:06:14 -0600

    Test instructors code to see if it works in netlify unlike my code

--- src/js/visitor-center.js
+++ src/js/visitor-center.js
@@ -1,55 +1,83 @@
+import "../css/style.css"; 
+import "../css/visitor-center.css";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { getParkData, getParkVisitorCenterDetails } from "./parkService.mjs";
 import {
-    vcInfoTemplate,
-    vcTitleTemplate,
-    vcDetailsTemplate,
-    vcAmenityTemplate,
-    vcImageTemplate,
-    listTemplate,
-    vcAddressesListTemplate,
-    vcDirectionsTemplate,
-    vcContactsTemplate
-  } from "./templates.mjs";
+  vcInfoTemplate,
+  vcTitleTemplate,
+  vcDetailsTemplate,
+  vcAmenityTemplate,
+  vcImageTemplate,
+  listTemplate,
+  vcAddressesListTemplate,
+  vcDirectionsTemplate,
+  vcContactsTemplate
+} from "./templates.mjs";
 
-  function getParameters(parameter) {
-    const search = location.search;
-    const parameters = new URLSearchParams(search);
-    return parameters.get(parameter);
+
+function getParam(param) {
+  const search = location.search;
+  const params = new URLSearchParams(search);
+  return params.get(param);
 }
-
-
 
 function buildPage(data) {
-    const updateElement = (selector, content) => {
-      document.querySelector(selector).innerHTML = content;
-    };
-    
-    updateElement(".vc-name", vcTitleTemplate(data.name));
-    updateElement(".vc-info", vcInfoTemplate(data));
-    
-    const detailsEl = document.querySelector(".vc-details-list");
-    detailsEl.innerHTML = "";
-
-    const insertDetail = (id, title, icon, content) => {
-      detailsEl.insertAdjacentHTML("beforeend", vcDetailsTemplate(id, title, icon, content));
-    };
-
-    insertDetail("vcAddresses", "Addresses", "heading-icon_map-pin", vcAddressesListTemplate(data.addresses));
-    insertDetail("vcDirections", "Directions", "directions", vcDirectionsTemplate(data.directionsInfo));
-    insertDetail("vcAmenities", "Amenities", "heading-icon_info", listTemplate(data.amenities, vcAmenityTemplate));
-    insertDetail("vcContacts", "Contacts", "phone", vcContactsTemplate(data.contacts));
-
-    document.querySelector(".vc-gallery").insertAdjacentHTML("beforeend", listTemplate(data.images, vcImageTemplate));
+  // since we have all the structure in place in the HTML, this solution chooses to insert content into the existing structure instead of generating all the structure with the content
+  document.querySelector(".vc-name").innerHTML = vcTitleTemplate(data.name);
+  document.querySelector(".vc-info").innerHTML = vcInfoTemplate(data);
+  const detailsEl = document.querySelector(".vc-details-list");
+  detailsEl.innerHTML = "";
+  // addresses section
+  const addressHTML = vcAddressesListTemplate(data.addresses);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcAddresses",
+      "Addresses",
+      "heading-icon_map-pin",
+      addressHTML
+    )
+  );
+  // directions
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcDirections",
+      "Directions",
+      "directions",
+      vcDirectionsTemplate(data.directionsInfo)
+    )
+  );
+  // amenities section.
+  const amenitiesHTML = listTemplate(data.amenities, vcAmenityTemplate);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate(
+      "vcAmenities",
+      "Amenities",
+      "heading-icon_info",
+      amenitiesHTML
+    )
+  );
+  // contact section
+  const contactHTML = vcContactsTemplate(data.contacts);
+  detailsEl.insertAdjacentHTML(
+    "beforeend",
+    vcDetailsTemplate("vcContacts", "Contacts", "phone", contactHTML)
+  );
+  // gallery section
+  const galleryHTML = listTemplate(data.images, vcImageTemplate);
+  document
+    .querySelector(".vc-gallery")
+    .insertAdjacentHTML("beforeend", galleryHTML);
 }
-  
-  async function init() {
-    const parkData = await getParkData();
-    const id = getParameters("id");
-    const centerDetails = await getParkVisitorCenterDetails(id);
-    setHeaderFooter(parkData);
-    buildPage(centerDetails);
-  }
-  
-  init();
 
+async function init() {
+  const parkData = await getParkData();
+  const id = getParam("id");
+  const centerDetails = await getParkVisitorCenterDetails(id);
+  setHeaderFooter(parkData);
+  buildPage(centerDetails);
+}
+
+init();
