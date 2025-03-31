366cd13dd61ba548c87412300b9287012bec5647
Hyrum Whittier <whittierht@gmail.com>
2025-03-30 22:49:02 -0600

    vite.config.js change fixes it?

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        conditions: resolve(__dirname, "src/conditions.html")
+        conditions: resolve(__dirname, "src/conditions.html"),
+        visitorCenter: resolve(__dirname, "src/visitor-center.html")
       }
     }
   }
